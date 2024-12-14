3a2c95d90de3a996e301101565e38038535c92cf
mj-medina <martin.justin04@gmail.com>
2024-12-14 21:07:36 +0800

    update docker-compose

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,19 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import svgLoader from 'vite-svg-loader';
 
-
 export default defineConfig({
   plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+      '@icons': path.resolve(__dirname, './src/assets/icons'),
+    },
+  },
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
